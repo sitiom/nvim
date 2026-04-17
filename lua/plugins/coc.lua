c4b8dad572538ebd701aa62e7138305e7b555ab5
Ryan Caezar Itang <sitiom@proton.me>
2026-04-17 21:08:53 +0800

    fix(coc): add termux check

--- lua/plugins/coc.lua
+++ lua/plugins/coc.lua
@@ -1,11 +1,14 @@
 local is_wsl = vim.fn.has("wsl") == 1
+local is_termux = vim.fn.has("termux") == 1
+  or vim.env.TERMUX_VERSION ~= nil
+  or vim.env.PREFIX == "/data/data/com.termux/files/usr"
 
 return {
   {
     "neoclide/coc.nvim",
     branch = "release",
     event = "VeryLazy",
-    enabled = not is_wsl,
+    enabled = not (is_wsl or is_termux),
     vscode = true,
     init = function()
       vim.g.coc_global_extensions = { "coc-discord-rpc" }
