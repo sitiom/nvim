d84bd18351a356a6ffd596b779897c9e81c42b16
Ryan Caezar Itang <sitiom@proton.me>
2026-04-17 20:56:25 +0800

    fix: disabled coc.nvim inside wsl

--- lua/plugins/coc.lua
+++ lua/plugins/coc.lua
@@ -1,8 +1,11 @@
+local is_wsl = vim.fn.has("wsl") == 1
+
 return {
   {
     "neoclide/coc.nvim",
     branch = "release",
     event = "VeryLazy",
+    enabled = not is_wsl,
     vscode = true,
     init = function()
       vim.g.coc_global_extensions = { "coc-discord-rpc" }
